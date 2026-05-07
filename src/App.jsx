16c1cdf52c3679a7aea802733efe6d56eafa889f
Ruben <dimeruben444@gmail.com>
2026-05-07 20:24:04 +0200

    feat(header sintaxis)

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,21 @@ import CartPage from "./pages/CartPage";
 function App() {
   return (
     <BrowserRouter basename="/react-market_pages">
-      <Header />
+      
+      <header className="container">
+       <Header /> 
+      </header>
+      
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </main>
+
+
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";//Para navegar entre paginas se usa link en vez de a
 import rmlogo from "../assets/rm-logo.svg";
+import logo from "/favicon-128.svg?url"
 //HEADER Compartido para todsa las paginas  con el logo el buscador y el boton del carrito 
 
 
@@ -9,10 +10,23 @@ function Header() {
 
   return (
     <>
-    <header>
-        <Link to="/"><img src={rmlogo} alt="react market logo" /></Link>
+    <header><div className='container'>
+    <nav className="header-content">
 
-    </header>
+
+      <Link to="/"><img src={logo} alt="react market logo" /> </Link>
+
+      
+      <div className="header-icons">
+        <Link to="/"><i className='icon-user'></i></Link>
+        <Link to="/"><i className='icon-cart'></i></Link>
+      </div>
+      
+
+
+
+    </nav>
+    </div></header>
     </>
   )
 }
